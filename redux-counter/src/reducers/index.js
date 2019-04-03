de6be0622bcc00ba755e5c8c852ae84c136cbce4
kyj945 <kyj_945@naver.com>
2019-04-03 13:27:45 +0900

    combineReducers를 사용

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -1,32 +1,10 @@
-import * as types from '../actions/ActionTypes';
+import number from './number';
+import color from './color';
+import { combineReducers } from 'redux';
 
-//초기 상태 값
-const initialState = {
-  color: 'black',
-  number: 0
-};
+const reducers = combineReducers({
+    numberData: number,
+    colorData: color
+});
 
-//변화를 일으키는 함수
-function counter(state = initialState, action) {
-    switch (action.type) {
-      case types.INCREMENT:
-        return {
-          ...state,
-          number: state.number + 1
-        };
-      case types.DECREMENT:
-        return {
-          ...state,
-          number: state.number - 1
-        };
-      case types.SET_COLOR:
-        return {
-          ...state,
-          color: action.color
-        };
-      default:
-        return state;
-    }
-};
-
-export default counter;
+export default reducers;
